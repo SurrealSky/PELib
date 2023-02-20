a2c767a3c07e0d83b7b1977a41bd3f630b44947c
SurrealSky <40445799+SurrealSky@users.noreply.github.com>
2023-02-20 20:16:34 +0800

    更新

---
+++ PEMake/GlobeVar.h
@@ -0,0 +1,34 @@
+//此处用于定义全局的宏定义
+#pragma once
+#include<Windows.h>
+#include<typedef.h>
+#include"../PE/PEMake.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "..\\bind\\PELib.lib")
+#else
+#pragma comment(lib, "..\\bin\\PELib.lib")
+#endif
+
+#include<MemoryPool/MemMgr.h>
+using namespace SurrealMemMgr;
+
+#ifdef _DEBUG
+#pragma comment(lib, "CommonLib\\MemoryPool\\bind\\MemMgr.lib")
+#else
+#pragma comment(lib, "CommonLib\\MemoryPool\\bin\\MemMgr.lib")
+#endif
+
+#ifdef UNICODE
+#define MAX_LENGTH 128
+#define MAX_FUNLENGTH  128
+#define MAX_HEADLENGTH	128
+#else
+#define MAX_LENGTH 128
+#define MAX_FUNLENGTH  128
+#define MAX_HEADLENGTH	128
+#endif
+
+
+
+
